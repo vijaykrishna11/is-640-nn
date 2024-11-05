e25ef1d4f6fde3b5deb37bdff1bf23cee60423ad
vijaykrishna11 <129128333+vijaykrishna11@users.noreply.github.com>
2024-11-04 22:21:22 -0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -5,10 +5,10 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-        # internal variables used for autograd graph construction
+        
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
